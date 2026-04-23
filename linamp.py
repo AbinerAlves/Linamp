5fa3e83d44a821337c5b89ab291622fb6a4738b4
Abienr Alves Paz <Abienr Alves Paz>
2026-04-23 17:10:08 -0300

    janela melhorada

--- linamp.py
+++ linamp.py
@@ -4,4 +4,9 @@ janela = tk.Tk()
 janela.title("Player")
 janela.geometry("300x200")
 
+def play():
+    print("Play")
+
+btn = tk.Button(janela, text="Play", command=play)
+btn.pack()
 janela.mainloop()
